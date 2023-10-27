67a45e1dec6b6c3667e35113629f016b4f6b0a34
劉秉樺 <alen920617@gmail.com>
2023-10-27 11:18:04 +0800

    設定controller回傳對應的view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class PostController extends BaseController
+{
+    public function index()
+    {
+        return view('index');
+    }
+    public function show()
+    {
+        return view('post');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+}
